e1b70b63f7c13518d7935d9139dea95d36e5d888
ARTHUR JUWER RAMBO <arthur.juwer99@gmail.com>
2024-07-02 10:40:52 -0300

    Aula 15, templates string

---
+++ aula-15/index.js
@@ -0,0 +1,5 @@
+let nome = prompt("Qual é seu nome? ")
+let corFavorita = prompt("Qual é sua cor favorita? ")
+
+console.log(`A cor favorita de ${nome}é ${corFavorita}`)
+console.log("A cor favorita de " + nome + "é " + corFavorita)
