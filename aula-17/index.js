edafc87a9b1e28ed913cbaa7f193d938f407752a
ARTHUR JUWER RAMBO <arthur.juwer99@gmail.com>
2024-07-03 09:46:25 -0300

    Exercicio Array

---
+++ aula-17/index.js
@@ -0,0 +1,6 @@
+// Crie um array com pelo menos 5 raças de cachoro
+let racasCachorro = ["Pinscher", "Bulldog", "Salsicha", "golden", "pastor alemão"]
+// Peça para o usuario insirir um numero de 0 a 4
+let resultadoNumero = prompt("Informe um numero entre 0 a 4")
+// Imprimira no console a raca correspondente a posicao escolhida pelo usuario
+console.log(`O raça de cachorro no index ${resultadoNumero} é ${racasCachorro[resultadoNumero]}`)
