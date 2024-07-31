b33228a5091cf661f74e68a6a01c7ccc1a50df8f
ARTHUR JUWER RAMBO <arthur.juwer99@gmail.com>
2024-07-31 09:16:07 -0300

    inicio de function em js

---
+++ aula-35/index.js
@@ -0,0 +1,18 @@
+function imprimirOlaMundo(){
+    console.log("Olá Mundo!")
+}
+imprimirOlaMundo()
+
+function calcularArea(altura, largura){
+    let area = altura * largura
+    console.log(`A area é ${area}`)
+}
+calcularArea(2,4)
+
+function mostrarNome(nome){
+    console.log(`Olá ${nome}`)
+}
+mostrarNome("Arthur")
+mostrarNome("Juwer")
+mostrarNome("Rambo")
+
